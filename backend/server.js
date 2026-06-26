24ad5d0d7bed3e8c12c168e6f43bcc2cb3410e35
Abhay0410 <kumarsinghabhay77@gmail.com>
2026-06-26 16:39:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
     allowedHeaders: [
       "Origin",
       "X-Requested-With",
