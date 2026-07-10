e991edb3538f8a2eaf7ab24faa6eec8e7a0c69d4
VVinaykumar003 <vvinaykumar2000@gmail.com>
2026-07-10 17:26:14 +0530

    I_M_C

--- frontend/src/assets/ProfessionalBodies/assets.js
+++ frontend/src/assets/ProfessionalBodies/assets.js
@@ -1,8 +1,8 @@
-import image_1 from "../../assets/professionalBodies/IOE.jpg"; // Corrected: This was pointing to the wrong file.
-import image_2 from "../../assets/professionalBodies/QCF.jpg";
-import image_3 from "../../assets/professionalBodies/ISFTE.jpg"; // Corrected: This is the ISTE certificate.
-import image_4 from "../../assets/professionalBodies/IOE.jpg";
-import image_5 from "../../assets/professionalBodies/IETE.jpg";
+import image_1 from "./IOE.jpg";
+import image_2 from "./QCF.jpg";
+import image_3 from "./ISFTE.jpg";
+import image_4 from "./IETE.jpg";
+import image_5 from "./ISOI.jpg";
 
 const images = [image_1, image_2, image_3, image_4, image_5];
 
