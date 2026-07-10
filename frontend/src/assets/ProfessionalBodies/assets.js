5e5309f217d8ea3d2727703907c6913f88b32e1a
VVinaykumar003 <vvinaykumar2000@gmail.com>
2026-07-10 17:18:37 +0530

    images name changed

--- frontend/src/assets/ProfessionalBodies/assets.js
+++ frontend/src/assets/ProfessionalBodies/assets.js
@@ -1,8 +1,8 @@
-import image_1 from "../../assets/professionalBodies/image_1.jpg"; // Corrected: This was pointing to the wrong file.
-import image_2 from "../../assets/professionalBodies/image_2.jpg";
-import image_3 from "../../assets/professionalBodies/image_3.jpg"; // Corrected: This is the ISTE certificate.
-import image_4 from "../../assets/professionalBodies/image_4.jpg";
-import image_5 from "../../assets/professionalBodies/image_5.jpg";
+import image_1 from "../../assets/professionalBodies/IOE.jpg"; // Corrected: This was pointing to the wrong file.
+import image_2 from "../../assets/professionalBodies/QCF.jpg";
+import image_3 from "../../assets/professionalBodies/ISFTE.jpg"; // Corrected: This is the ISTE certificate.
+import image_4 from "../../assets/professionalBodies/IOE.jpg";
+import image_5 from "../../assets/professionalBodies/IETE.jpg";
 
 const images = [image_1, image_2, image_3, image_4, image_5];
 
